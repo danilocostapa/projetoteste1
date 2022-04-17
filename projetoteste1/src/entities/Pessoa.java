f8f5d4ed2dda3fd6ba45b68e9964627a7af2dad2
Danilo Costa <bass.danilo@gmail.com>
2022-04-16 23:30:34 -0300

    Alteração e criação da classe Pessoa

---
+++ projetoteste1/src/entities/Pessoa.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Pessoa {
+	
+	/*Atributos*/
+	private String nome;
+	private String sobrenome;
+	
+	/*gets e sets*/
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+}
